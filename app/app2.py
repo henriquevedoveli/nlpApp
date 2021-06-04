e41428dad33eef813a89de6c33abe9cb1cb4d335
Henrique Vedoveli <61060105+henriquevedoveli@users.noreply.github.com>
2021-06-04 12:10:33 -0300

    Update app2.py

--- app/app2.py
+++ app/app2.py
@@ -8,8 +8,8 @@ emojiHand = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/1
 emojiText = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/274/scroll_1f4dc.png'
 
 
-with zipfile.ZipFile("model_df.zip","r") as zip_ref:
-    zip_ref.extractall("app/")
+with zipfile.ZipFile("https://github.com/henriquevedoveli/nlpApp/blob/main/app/model_df.zip?raw=true","r") as zip_ref:
+    zip_ref.extractall("nlpApp/app/")
 
 @st.cache
 def loadModel(path):
